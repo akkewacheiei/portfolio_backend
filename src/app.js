ff9572f1d6524d6713978451d9bd756dff9f4063
Broy <akkewach.yodsomboon@gmail.com>
2025-08-31 22:16:30 +0700

    chore: Backend is running!!!!

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ mongoose
 
 // Test route
 app.get("/", (req, res) => {
-  res.json({ message: "Backend is running!!! 🚀" });
+  res.json({ message: "Backend is running!!!! 🚀" });
 });
 
 export default app;
